b436270e0fdd87240a2e819ed4430d2345696273
duowen1314 <duo_eweng@163.com>
2019-09-02 20:57:13 +0800

    新建一级路由并配置和写登录组件结构

--- src/main.js
+++ src/main.js
@@ -1,6 +1,11 @@
 import Vue from 'vue'
 import App from './App.vue'
 import router from './router'
+import Element from 'element-ui'
+import 'element-ui/lib/theme-chalk/index.css'
+import './style/index.less'
+
+Vue.use(Element)
 
 Vue.config.productionTip = false
 
---
+++ src/views/login/index.vue
@@ -0,0 +1,53 @@
+<template>
+  <div class="loginApp">
+      <el-card class="login-card">
+          <h2 class="logo">
+              <img src="../../assets/img/logo_index.png" alt="">
+          </h2>
+          <el-form>
+              <el-form-item>
+                  <el-input placeholder="请输入手机号"></el-input>
+              </el-form-item>
+              <el-form-item>
+                  <el-input placeholder="请输入验证码" style="width:240px;"></el-input>
+                  <el-button style="float:right">发送验证码</el-button>
+              </el-form-item>
+              <el-form-item>
+                  <el-checkbox>我已阅读并同意用户协议和隐私条款</el-checkbox>
+              </el-form-item>
+              <el-form-item>
+                  <el-button type="primary" style="width:100%;">登录</el-button>
+              </el-form-item>
+          </el-form>
+      </el-card>
+  </div>
+
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style lang='less' scoped>
+    .loginApp{
+        width:100%;
+        height: 100vh;
+        background:url(../../assets/img/login_bg.jpg) no-repeat;
+        background-size:cover;
+        display: flex;
+        justify-content: center;
+        align-items: center;
+        .login-card{
+            width:400px;
+            height: 360px;
+            .logo{
+                text-align: center;
+                img{
+                    height: 40px;
+                }
+            }
+        }
+    }
+</style>
